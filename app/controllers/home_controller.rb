ef90059b95dd86f6ea424bd452c1528f4c370cf3
Krishna Prashanth T <krishnaprashanth.t@tcs.com>
2013-08-12 23:52:32 +0530

    After user authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
-  def index
-  end
+
+	def index
+
+		@count_users = User.count_users
+  
+		if @count_users == 0
+
+			puts "\n\n\n\ncount users \n\n\n\n"	
+
+			email = "krishnaprashanth.csse@gmail.com"
+			password = "12345678"
+			role = "admin"
+
+			User.add_user(email, password, role)
+
+		end
+
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+	# Include default devise modules. Others available are:
+  	# :token_authenticatable, :confirmable,
+  	# :lockable, :timeoutable and :omniauthable
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  	# Setup accessible (or protected) attributes for your model
+  	attr_accessible :email, :password, :password_confirmation, :remember_me, :role
+  	# attr_accessible :title, :body
+
+  	def self.add_user(email, password, role)
+		User.create(:email => email, :password => password, :role => role)    
+	end
+  
+	def self.count_users
+		User.count	
+	end
+
 end
